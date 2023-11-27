1a28368495f690724de0e4c80397a8cf7cf6f6a3
ravangozalov <revanbulud@gmail.com>
2023-11-27 17:49:27 +0400

    fetch data

---
+++ script/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let allProducts = []; 
+    let currentPage = 1;
+
+    fetch("https://dummyjson.com/products")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      allProducts = data.products;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+});
